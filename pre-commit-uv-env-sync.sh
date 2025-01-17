1e76d4fda7f2c617174a47a12e9c5bbc47767b60
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2025-01-17 16:00:41 +0000

    fix: pyjwt security issues
    Signed-off-by: binaryYuki <60097976+binaryYuki@users.noreply.github.com>

--- pre-commit-uv-env-sync.sh
+++ pre-commit-uv-env-sync.sh
@@ -2,9 +2,7 @@
 
 echo "Running pre-commit hook"
 
-uv pip freeze > requirements.txt
-uv add --requirements requirements.txt
-rm requirements.txt
+uv pip compile pyproject.toml -o requirements.txt
 echo "uv env sync successfully"
 
 echo "pre-commit hook finished"
